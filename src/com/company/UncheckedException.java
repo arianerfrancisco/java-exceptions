707636e3a102e147aa13d50a47b50348458af7db
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-01-31 14:32:41 -0300

    alteracoes

--- src/com/company/UncheckedException.java
+++ src/com/company/UncheckedException.java
@@ -7,10 +7,21 @@ public class UncheckedException {
         String a = JOptionPane.showInputDialog("Numerador: ");
         String b = JOptionPane.showInputDialog("Denominador: ");
 
-        int resultado = dividir(Integer.parseInt(a),Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        try {
+            int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+            System.out.println("Resultado: " + resultado);
+        } catch (NumberFormatException e) {
+            JOptionPane.showInputDialog(null, " Entrada Inválida, informe um número inteiro", e.getMessage());
+            // evitar o e.getMessage() ao usuario
+           // e.printStackTrace();
+        } catch (ArithmeticException e) {
 
+        } finally {
+            System.out.println("Chegou ao finally");
+        }
+        System.out.println("Chegou até aqui");
     }
-
-    public static int dividir (int a, int b) { return a/ b; } ;
+    public static int dividir(int a, int b) {
+        return a / b;
+    };
 }
